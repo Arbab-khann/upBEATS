41a6c3c3acc3b35d9418f619e00ee4844247de6d
arbab <arbabkhan4u.ak.ak@gmail.com>
2024-01-07 18:21:49 +0530

    first commit

--- frontend/components/HeroBanner.jsx
+++ frontend/components/HeroBanner.jsx
@@ -53,7 +53,7 @@ const HeroBanner = ({ heroBanner }) => {
 
         <div>
           <Link href={`/product/${heroBanner.product}`}>
-            <button type="button">{heroBanner.buttonText}</button>
+            <button type="button" >{heroBanner.buttonText}</button>
           </Link>
           <div className="desc">
             {/* <h5>Description</h5> */}
